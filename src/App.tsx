ffa3f1cc550c42ac47be0b72c7ee70fe055c0f7a
Automated Release Test <release-tests-no-reply@zfnd.org>
2024-02-28 19:01:12 -0500

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-// import { createSignal } from "solid-js";
-// import logo from "./assets/logo.svg";
-// import { listen } from "@tauri-apps/api/event";
+import { createSignal, onCleanup, onMount } from "solid-js";
+import { listen, Event, UnlistenFn } from "@tauri-apps/api/event";
 import "./App.css";
 
 function App() {
+  let stop_listening: UnlistenFn;
+
+  const [logs, set_logs] = createSignal<Array<string>>([]);
+
+  onMount(async () => {
+    stop_listening = await listen("log", (event: Event<string>) =>
+      set_logs([...logs(), event.payload])
+    );
+  });
+
+  onCleanup(() => stop_listening());
 
   return (
     <div class="container">
+      {logs().map((log) => (
+        <code>{log}</code>
+      ))}
     </div>
   );
 }
