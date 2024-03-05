5706ef65f2fa77b40bfca2ae1a51f969d3ad1983
Automated Release Test <release-tests-no-reply@zfnd.org>
2024-03-05 18:51:18 -0500

    adds edit mode

--- src/pages/Configure.tsx
+++ src/pages/Configure.tsx
@@ -10,25 +10,55 @@ const PageContainer = styled("div")`
   font-family: sans-serif;
 `;
 
+const ConfigTextArea = styled("textarea")``;
+
+const ConfigDisplay = ({ children }: { children: string }) => {
+  return <code>{children}</code>;
+};
+
 const Configuration = () => {
   const [config_contents, set_config_contents] = createSignal<string>("");
+  const [edited_config, set_edited_config] = createSignal<string | null>(null);
 
   onMount(async () => {
     set_config_contents(await invoke("read_config"));
   });
 
   const save_and_apply = () => {
-    invoke("save_config", { new_config: config_contents() });
+    invoke("save_config", { new_config: edited_config() });
   };
+
+  const discard_changes = () => {
+    set_edited_config(null);
+  };
+
+  const start_editing = () => {
+    set_edited_config("");
+  };
+
+  const is_editable = () => edited_config() !== null;
 
   return (
     <PageContainer>
       <h1>Configuration</h1>
-      <textarea
-        value={config_contents()}
-        onChange={({ currentTarget: { value } }) => set_config_contents(value)}
-      />
-      <button onClick={save_and_apply}>Save & Apply</button>
+
+      {is_editable() ? (
+        <>
+          <ConfigTextArea
+            value={edited_config() || ""}
+            onChange={({ currentTarget: { value } }) =>
+              set_edited_config(value)
+            }
+          />{" "}
+          <button onClick={discard_changes}>Discard Changes</button>
+          <button onClick={save_and_apply}>Save & Apply</button>
+        </>
+      ) : (
+        <>
+          <ConfigDisplay>{config_contents()}</ConfigDisplay>
+          <button onClick={start_editing}>Edit</button>
+        </>
+      )}
     </PageContainer>
   );
 };
