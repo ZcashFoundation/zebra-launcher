3417a604bacaa55576cef92cc1e91bca2ec1cf11
Automated Release Test <release-tests-no-reply@zfnd.org>
2024-07-10 18:15:33 -0400

    reverts changes to tauri.conf and zebrad_bin_path(), updates build script to add zebrad binary to binaries directory

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,28 @@
 //! Build script for zebra-app
 
 fn main() {
+    let zebrad_binary_path = std::env::var_os("CARGO_BIN_FILE_ZEBRAD_zebrad")
+        .expect("zebrad binary should be compiled as an artifact dependency");
+
+    let target_platform = std::env::var_os("TARGET")
+        .expect("must have a build target")
+        .into_string()
+        .expect("target should successfully convert into string");
+
+    let zebrad_external_bin_dir_path = std::env::current_dir()
+        .expect("should have a current directory")
+        .join("binaries");
+
+    std::fs::create_dir_all(&zebrad_external_bin_dir_path)
+        .expect("must have permission to create binaries directory if it missing");
+
+    let zebrad_external_bin_path =
+        zebrad_external_bin_dir_path.join(format!("zebrad-{target_platform}"));
+
+    if !std::fs::exists(&zebrad_external_bin_path).expect("must check if file exists") {
+        std::fs::copy(zebrad_binary_path, zebrad_external_bin_path)
+            .expect("build must copy zebrad into external binary directory");
+    }
+
     tauri_build::build()
 }
